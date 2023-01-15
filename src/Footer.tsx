d19d8b906261968bf0d415fba700a2aa55cf89cc
Thomas Crowe <wcrowe@g.clemson.edu>
2023-01-15 10:32:14 -0500

    Filled in social links

--- src/Footer.tsx
+++ src/Footer.tsx
@@ -5,11 +5,11 @@ function Footer() {
     return (
         <div className="footer-container p-6">
             <ul>
-                <li><a href="*"><i className="fab fa-github"></i></a></li>
-                <li><a href="*"><i className="fab fa-linkedin"></i></a></li>
-                <li><a href="*"><i className="fab fa-facebook"></i></a></li>
-                <li><a href="*"><i className="fab fa-instagram"></i></a></li>
-                <li><a href="*"><i className="fab fa-twitter"></i></a></li>
+                <li><a target='_blank' rel="noreferrer" href="https://github.com/wtcrowe4"><i className="fab fa-github"></i></a></li>
+                <li><a target='_blank' rel="noreferrer" href="https://www.linkedin.com/in/wthomascrowe"><i className="fab fa-linkedin"></i></a></li>
+                <li><a target='_blank' rel="noreferrer" href="https://www.facebook.com/tcrowe4"><i className="fab fa-facebook"></i></a></li>
+                <li><a target='_blank' rel="noreferrer" href="https://www.instagram.com/tcroweiv"><i className="fab fa-instagram"></i></a></li>
+                <li><a target='_blank' rel="noreferrer" href="https://twitter.com/tcrowe_iv"><i className="fab fa-twitter"></i></a></li>
             </ul>
         </div>
     )
