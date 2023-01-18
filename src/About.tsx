9e286124c33e118b55231f7e4cc7509704c7df26
Thomas Crowe <wcrowe@g.clemson.edu>
2023-01-18 12:01:33 -0500

    More styling

--- src/About.tsx
+++ src/About.tsx
@@ -10,7 +10,7 @@ function About() {
             </div>
             <div className="about-content grid grid-cols-2 row-auto m-50 p-10">
                 <div className="col-span-1 m-20 about-content-section">
-                    <h2 className="text-white text-2xl">Personal Life</h2>
+                    <h2 className="text-white text-2xl about-sub-headers">Personal Life</h2>
                     <p className="text-white">I am a 32 year old full stack web developer from Greenville, SC. I am recently married and enjoy playing golf and chess in my free time.</p>
                 </div>
                 <div className="col-span-1 m-20 about-content-section">
@@ -20,14 +20,18 @@ function About() {
                     <img src="#" alt="professional"></img>
                 </div>  
                 <div className="col-span-1 m-20 about-content-section">
-                    <h2 className="text-white text-2xl">Professional Life</h2>
+                    <h2 className="text-white text-2xl about-sub-headers">Professional Life</h2>
                     <p className="text-white">I have over 10 years experience in the service industry, most of them in a management or leadership role.  I am a motivated and driven individual ready to start writing code professionally.</p>
                 </div>
                 <div className="col-span-1 m-20 about-content-section">
-                    <h2 className="text-white text-2xl">Skills</h2>
+                    <h2 className="text-white text-2xl about-sub-headers">Skills</h2>
                     <p className="text-white">
                         <ul>
-                            <li></li>
+                            <li>Motivated</li>
+                            <li>Driven</li>
+                            <li>Quick Learner</li>
+                            <li>Team Player</li>
+                            <li>Problem Solver</li>
                         </ul>
                     </p>
                 </div>
@@ -38,11 +42,11 @@ function About() {
                     <img src="#" alt="education"></img>
                 </div>
                 <div className="col-span-1 m-20 about-content-section">
-                    <h2 className="text-white text-2xl">Education</h2>
+                    <h2 className="text-white text-2xl about-sub-headers">Education</h2>
                     <p className="text-white">I studied Bichemistry at the Honors College at College of Charleston before realizing my passion for computers.  I transferred to Clemson University and studied Computer Engineering.  As I matured I realized my talents as a programmer and decided to go back to school at 32 and complete a Software Development Program at N.C. State University. </p>
                 </div>
                 <div className="col-span-1 m-20 about-content-section">
-                    <h2 className="text-white text-2xl">Main Technology</h2>
+                    <h2 className="text-white text-2xl about-sub-headers">Main Technology</h2>
                     <p className="text-white">
                         My main tech stack is:
                         <ul>
@@ -66,7 +70,7 @@ function About() {
                     </p>
                 </div>
                 <div className="col-span-1 m-20 about-content-section">
-                    <h2 className="text-white text-2xl">Secondary Technology</h2>
+                    <h2 className="text-white text-2xl about-sub-headers">Secondary Technology</h2>
                     <p className="text-white">Other tech stacks I have experimented with:
                         <ul>
                             <li>Jupyter Notebook</li>
