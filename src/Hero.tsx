01b80955ce6de55b3ff4e5faaeb84e48fd3eb0e3
Thomas Crowe <wcrowe@g.clemson.edu>
2023-01-19 08:32:33 -0500

    removed duplicate

--- src/Hero.tsx
+++ src/Hero.tsx
@@ -1,15 +1,10 @@
 import React from 'react';
-import { Circles } from 'react-loader-spinner';
+
 
 function Hero() {
     return (
 
-        
-
         <div className="hero-container">
-            <div className="loading-spinner flex justify-center items-center">
-                <Circles height='150' width='150' color='black'ariaLabel="circles-loading" />
-            </div>
             <div className="hero-text">
                 <h1 className="text-white">Thomas Crowe</h1>
                 <h2 className="text-white">Full Stack Web Developer</h2>
