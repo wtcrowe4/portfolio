a2c58541440575b031fa567335772e3d2e85f655
Thomas Crowe <wcrowe@g.clemson.edu>
2023-01-25 15:54:29 -0500

    Setting up backend for mongodb

---
+++ server.js
@@ -0,0 +1,22 @@
+import express from 'express';
+import dotenv from 'dotenv';
+import moongoose from 'mongoose';
+
+dotenv.config();
+
+const app = express();
+
+//database connection
+const db = process.env.MONGO_URI + process.env.MONGO_DBNAME;
+moongoose.connect(db, { useNewUrlParser: true, useUnifiedTopology: true })
+    .then(() => console.log('MongoDB Connected...'))
+
+
+app.get('/', (req, res) => {
+    
+    res.send('Hello World!');
+    });
+
+
+const PORT = process.env.PORT;
+app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
