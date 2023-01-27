8198b65ae03956248760175d84802dd0bc0de690
Thomas Crowe <wcrowe@g.clemson.edu>
2023-01-27 00:09:19 -0500

    testing db

--- server.js
+++ server.js
@@ -8,6 +8,19 @@ const db = process.env.MONGO_URI
 mongoose.set('strictQuery', true)
 mongoose.connect(db, () => {console.log('MongoDB Connected...')})
     
+//testimonial schema
+const testimonialSchema = new mongoose.Schema({
+    name: String,
+    testimonial: String
+})
+const Testimonial = mongoose.model('Testimonial', testimonialSchema)
+const data = new Testimonial({
+    name: 'John Doe',
+    testimonial: 'This is a testimonial'
+})
+data.save()
+
+
 
 app.get('/', (req, res) => {
     res.send('Backend Root Route');
@@ -16,3 +29,4 @@ app.get('/', (req, res) => {
 
 const PORT = process.env.PORT;
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
+
