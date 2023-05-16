8ad6bb975d19d15615d8cbfaac636e40c149b057
Thomas Crowe <wcrowe@g.clemson.edu>
2023-05-16 06:53:47 -0400

    Added discord to projects

--- src/Pages/Projects.tsx
+++ src/Pages/Projects.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import holdem from '../assets/screenshots/holdem_screenshot.png'
 import reactSports from '../assets/screenshots/sports_screenshot.png'
+import discord from '../assets/screenshots/discord_screenshot.png'
 
 function Projects(): React.ReactElement {
     return (
@@ -18,17 +19,17 @@ function Projects(): React.ReactElement {
                 </div>
                 <div className="col-span-1 m-20 projects-content-section">
                     <h2 className="text-white text-4xl pb-5">Sports Book</h2>
-                    <p className="text-white text-2xl">This is one of the first React projects I worked on.  It uses current data from an API through RapidAPI.</p>
+                    <p className="text-white text-2xl">This is one of the first React projects I worked on.  It uses current data from an API through RapidAPI, giving the user real time scores and odds on the most popular sports.</p>
                 </div>
                 <div className="col-span-1 m-20 projects-content-section">
                     <img src={reactSports} alt="project2 screenshot"></img>
                 </div>
                 <div className="col-span-1 m-20 projects-content-section">
-                    <h2 className="text-white text-4xl pb-5">Project 3</h2>
-                    <p className="text-white text-2xl">This is a project I worked on.</p>
+                    <h2 className="text-white text-4xl pb-5">Discord</h2>
+                    <p className="text-white text-2xl">In this project I branched out and used Django for the first time.  I created a discord clone which uses SQL on the backend to store user's data and messages.</p>
                 </div>
                 <div className="col-span-1 m-20 projects-content-section">
-                    <img src="#" alt="project3 screenshot"></img>
+                    <img src={discord} alt="project3 screenshot"></img>
                 </div>
                 <div className="col-span-1 m-20 projects-content-section">
                     <h2 className="text-white text-4xl pb-5">Project 4</h2>
