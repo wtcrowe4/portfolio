9299b13e57cf98dbc4db5f73dcdb6158c24908c0
Thomas Crowe <wcrowe@g.clemson.edu>
2023-05-29 16:31:22 -0400

    tailwind cleanup

--- src/Pages/Projects.tsx
+++ src/Pages/Projects.tsx
@@ -38,49 +38,49 @@ function Projects(): React.ReactElement {
                     <h2 className={textStyle({ size: 'header' })}>Texas Hold'em</h2>
                     <p className={textStyle({ size: 'content' })}>This was my very first project. It is a simple frontend browser game styled with SCSS and currently being converted to Typescript.</p>
                 </div>
-                <div className="col-span-1 m-20 projects-picture-section">
+                <div className={sectionStyle()}>
                     <img className='p-0' src={holdem} alt="project1 screenshot"></img>
                 </div>
-                <div className="col-span-1 m-20 projects-content-section">
-                    <h2 className="text-white text-4xl pb-5">Sports Book</h2>
-                    <p className="text-white text-2xl">This is one of the first React projects I worked on.  It uses current data from an API through RapidAPI, giving the user real time scores and odds on the most popular sports.</p>
+                <div className={sectionStyle()}>
+                    <h2 className={textStyle({ size: 'header' })}>Sports Book</h2>
+                    <p className={textStyle({ size: 'content' })}>This is one of the first React projects I worked on.  It uses current data from an API through RapidAPI, giving the user real time scores and odds on the most popular sports.</p>
                 </div>
-                <div className="col-span-1 m-20 projects-content-section">
+                <div className={sectionStyle()}>
                     <img src={reactSports} alt="project2 screenshot"></img>
                 </div>
-                <div className="col-span-1 m-20 projects-content-section">
-                    <h2 className="text-white text-4xl pb-5">Discord</h2>
-                    <p className="text-white text-2xl">In this project I branched out and used Django for the first time.  I created a discord clone which uses SQL on the backend to store user's data and messages.</p>
+                <div className={sectionStyle()}>
+                    <h2 className={textStyle({ size: 'header' })}>Discord</h2>
+                    <p className={textStyle({ size: 'content' })}>In this project I branched out and used Django for the first time.  I created a discord clone which uses SQL on the backend to store user's data and messages.</p>
                 </div>
-                <div className="col-span-1 m-20 projects-content-section">
+                <div className={sectionStyle()}>
                     <img src={discord} alt="project3 screenshot"></img>
                 </div>
-                <div className="col-span-1 m-20 projects-content-section">
-                    <h2 className="text-white text-4xl pb-5">Music Library</h2>
-                    <p className="text-white text-2xl">This is a simple React project I worked on to learn and utilize Redux, APIs, and manipulating the DOM without reloading the page.</p>
+                <div className={sectionStyle()}>
+                    <h2 className={textStyle({ size: 'header' })}>Music Library</h2>
+                    <p className={textStyle({ size: 'content' })}>This is a simple React project I worked on to learn and utilize Redux, APIs, and manipulating the DOM without reloading the page.</p>
                 </div>
-                <div className="col-span-1 m-20 projects-content-section">
+                <div className={sectionStyle()}>
                     <img src={music} alt="project4 screenshot"></img>
                 </div>
-                <div className="col-span-1 m-20 projects-content-section">
-                    <h2 className="text-white text-4xl pb-5">Monkey Finder</h2>
-                    <p className="text-white text-2xl">This was my first .NET MAUI application, using a simple API to fetch information about different monkeys and where they originate from.</p>
+                <div className={sectionStyle()}>
+                    <h2 className={textStyle({ size: 'header' })}>Monkey Finder</h2>
+                    <p className={textStyle({ size: 'content' })}>This was my first .NET MAUI application, using a simple API to fetch information about different monkeys and where they originate from.</p>
                 </div>
-                <div className="col-span-1 m-20 projects-content-section">
+                <div className={sectionStyle()}>
                     <img src={monkey} alt="project5 screenshot"></img>
                 </div>
-                <div className="col-span-1 m-20 projects-content-section">
-                    <h2 className="text-white text-4xl pb-5">MAUI Finance</h2>
-                    <p className="text-white text-2xl">This is a .NET MAUI application that uses a financial API to fetch stock information and display it to the user, with the ability to search and create a watchlist of favorites.</p>
+                <div className={sectionStyle()}>
+                    <h2 className={textStyle({ size: 'header' })}>MAUI Finance</h2>
+                    <p className={textStyle({ size: 'content' })}>This is a .NET MAUI application that uses a financial API to fetch stock information and display it to the user, with the ability to search and create a watchlist of favorites.</p>
                 </div>
-                <div className="col-span-1 m-20 projects-content-section">
+                <div className={sectionStyle()}>
                     <img src={finance} alt="project6 screenshot"></img>
                 </div> 
-                <div className="col-span-1 m-20 projects-content-section">
-                    <h2 className="text-white text-4xl pb-5">Crowe Realty</h2>
-                    <p className="text-white text-2xl">This is an Angular/Typescript project I'm working on that uses a real estate API to fetch information about different properties and display it to the user, with the ability to add your own properties to the site.</p>
+                <div className={sectionStyle()}>
+                    <h2 className={textStyle({ size: 'header' })}>Crowe Realty</h2>
+                    <p className={textStyle({ size: 'content' })}>This is an Angular/Typescript project I'm working on that uses a real estate API to fetch information about different properties and display it to the user, with the ability to add your own properties to the site.</p>
                 </div>
-                <div className="col-span-1 m-20 projects-content-section">
+                <div className={sectionStyle()}>
                     <img src={realty} alt="project7 screenshot"></img>
                 </div>
             </div>
