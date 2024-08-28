f6b4a87106c7e81d5ee1011419c71890f2c202d4
Thomas Crowe <wtcrowe4@outlook.com>
2024-08-28 14:37:34 -0400

    improvements to mobile styling, updated resume and twitter icon

--- src/Components/Footer.tsx
+++ src/Components/Footer.tsx
@@ -8,7 +8,7 @@ function Footer() {
                 <li><a target='_blank' rel="noreferrer" href="https://www.linkedin.com/in/wthomascrowe"><i className="fab fa-linkedin"></i></a></li>
                 <li><a target='_blank' rel="noreferrer" href="https://www.facebook.com/tcrowe4"><i className="fab fa-facebook"></i></a></li>
                 <li><a target='_blank' rel="noreferrer" href="https://www.instagram.com/tcroweiv"><i className="fab fa-instagram"></i></a></li>
-                <li><a target='_blank' rel="noreferrer" href="https://twitter.com/tcrowe_iv"><i className="fab fa-twitter"></i></a></li>
+                <li><a target='_blank' rel="noreferrer" href="https://twitter.com/tcrowe_iv"><i className="fab fa-x-twitter"></i></a></li>
             </ul>
         </div>
     )
